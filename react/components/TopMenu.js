d44b918905e9db15b68f2676fdf704cd01a3730a
Luciano Júnior <luciannojunior@gmail.com>
2018-09-19 15:20:41 -0300

    Fixing media query

--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -19,7 +19,7 @@ class TopMenu extends Component {
 
   renderLogo(mobileMode, logoUrl, logoTitle) {
     return (
-      <div className="vtex-top-menu__logo w-20-m flex justify-start">
+      <div className="vtex-top-menu__logo flex justify-start">
         <Link to="/" className="outline-0">
           <ExtensionPoint
             id="logo"
@@ -82,7 +82,7 @@ class TopMenu extends Component {
         'fixed shadow-5 top-0 z-999': fixed,
       }
     )
-    const contentClasses = 'w-100 w-90-m w-80-xl center flex justify-center pb4 pv2-m pv6-l ph3-s ph7-m ph6-xl'
+    const contentClasses = 'w-100 w-90-l center flex justify-center pb4 pv2-m pv6-l ph3-s ph7-m ph6-xl'
     return (
       <ReactResizeDetector handleWidth>
         {
