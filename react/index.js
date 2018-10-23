00ec21b61c819576f5c9efabd03a173b1a6573d3
Luciano Júnior <luciannojunior@gmail.com>
2018-10-23 15:53:33 -0300

    Refacting

--- react/index.js
+++ react/index.js
@@ -1,26 +1,25 @@
-import React, { Component, Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { injectIntl, intlShape } from 'react-intl'
-import hoistNonReactStatics from 'hoist-non-react-statics'
+import React, { Component, Fragment } from "react"
+import PropTypes from "prop-types"
+import { injectIntl, intlShape } from "react-intl"
+import hoistNonReactStatics from "hoist-non-react-statics"
 
-import Modal from './components/Modal'
-import TopMenu from './components/TopMenu'
+import Modal from "./components/Modal"
+import TopMenu from "./components/TopMenu"
 
-import { Alert } from 'vtex.styleguide'
-import { ExtensionPoint, withRuntimeContext } from 'render'
+import { Alert } from "vtex.styleguide"
+import { ExtensionPoint, withRuntimeContext } from "render"
 
 import {
   orderFormConsumer,
-  contextPropTypes,
-} from 'vtex.store/OrderFormContext'
+  contextPropTypes
+} from "vtex.store/OrderFormContext"
 
-import './global.css'
+import "./global.css"
 
 class Header extends Component {
   state = {
     showMenuPopup: false,
-    leanMode: true,
-    lastPage: null
+    leanMode: true
   }
 
   static propTypes = {
@@ -29,39 +28,33 @@ class Header extends Component {
     logoTitle: PropTypes.string,
     leanWhen: PropTypes.string,
     intl: intlShape.isRequired,
-    orderFormContext: contextPropTypes,
+    orderFormContext: contextPropTypes
   }
 
   /** Determines an unmatching regex for default behavior of the leanMode */
   static defaultProps = {
-    leanWhen: 'a^'
+    leanWhen: "a^"
   }
 
   _root = React.createRef()
 
   componentDidMount() {
-    document.addEventListener('scroll', this.handleScroll)
+    document.addEventListener("scroll", this.handleScroll)
 
     this.handleScroll()
   }
 
-  componentDidUpdate() {
-    const { runtime: { page: currentPage }} = this.props
-    this.verifyModeByPage(currentPage)
-  }
-
-  verifyModeByPage = (page) => {
-    const { leanWhen } = this.props
-    const { lastPage } = this.state
-
-    if (page === lastPage) return 
-
+  isLeanMode = () => {
+    const {
+      leanWhen,
+      runtime: { page }
+    } = this.props
     const acceptedPaths = new RegExp(leanWhen)
-    this.setState({leanMode: acceptedPaths.test(page), lastPage: page})
+    return acceptedPaths.test(page)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('scroll', this.handleScroll)
+    document.removeEventListener("scroll", this.handleScroll)
   }
 
   handleScroll = () => {
@@ -74,23 +67,24 @@ class Header extends Component {
 
     if (scroll < scrollHeight && this.state.showMenuPopup) {
       this.setState({
-        showMenuPopup: false,
+        showMenuPopup: false
       })
     } else if (scroll >= scrollHeight) {
       this.setState({
-        showMenuPopup: true,
+        showMenuPopup: true
       })
     }
   }
 
   render() {
     const { logoUrl, logoTitle, orderFormContext } = this.props
-    const { showMenuPopup, leanMode } = this.state
+    const { showMenuPopup } = this.state
 
+    const leanMode = this.isLeanMode()
     const offsetTop = (this._root.current && this._root.current.offsetTop) || 0
 
     const hasMessage =
-      orderFormContext.message.text && orderFormContext.message.text !== ''
+      orderFormContext.message.text && orderFormContext.message.text !== ""
 
     return (
       <Fragment>
@@ -102,7 +96,11 @@ class Header extends Component {
           <div className="z-2 items-center w-100 top-0 bg-white tl">
             <ExtensionPoint id="menu-link" />
           </div>
-          <TopMenu logoUrl={logoUrl} logoTitle={logoTitle} leanMode={leanMode}/>
+          <TopMenu
+            logoUrl={logoUrl}
+            logoTitle={logoTitle}
+            leanMode={leanMode}
+          />
           {!leanMode && <ExtensionPoint id="category-menu" />}
           {showMenuPopup && (
             <Modal>
@@ -122,7 +120,7 @@ class Header extends Component {
               <div className="pa2 mw9">
                 <Alert
                   type={
-                    orderFormContext.message.isSuccess ? 'success' : 'error'
+                    orderFormContext.message.isSuccess ? "success" : "error"
                   }
                 >
                   {orderFormContext.message.text}
@@ -137,18 +135,20 @@ class Header extends Component {
 }
 
 Header.schema = {
-  title: 'editor.header.title',
-  description: 'editor.header.description',
-  type: 'object',
+  title: "editor.header.title",
+  description: "editor.header.description",
+  type: "object",
   properties: {
     logoUrl: {
-      type: 'string',
-      title: 'editor.header.logo.title',
+      type: "string",
+      title: "editor.header.logo.title",
       widget: {
-        'ui:widget': 'image-uploader',
-      },
+        "ui:widget": "image-uploader"
+      }
     }
-  },
+  }
 }
 
-export default withRuntimeContext(hoistNonReactStatics(orderFormConsumer(injectIntl(Header)), Header)) 
+export default withRuntimeContext(
+  hoistNonReactStatics(orderFormConsumer(injectIntl(Header)), Header)
+)
