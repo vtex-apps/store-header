e2a8fd5e2de20f0a98cae8af660df512976177fd
Lucas Bebber <lucasbbebber@gmail.com>
2018-12-11 13:30:48 -0200

    Include cancel button on search bar component

--- react/components/SearchBar.js
+++ react/components/SearchBar.js
@@ -4,40 +4,46 @@ import { FormattedMessage } from 'react-intl'
 import { Adopt } from 'react-adopt'
 import { ExtensionPoint } from 'render'
 
-const SearchBar = ({mobileMode, fixed, compactMode, autoFocus}) => {
-  const searchBar = (
-    <Adopt
-      mapper={{
-        placeholder: <FormattedMessage id='header.search-placeholder' />,
-        emptyPlaceholder: <FormattedMessage id='header.search-emptyPlaceholder' />,
-      }}
-    >
-      {({placeholder, emptyPlaceholder}) => (
-        <ExtensionPoint
-          id="search-bar"
-          placeholder={placeholder}
-          emptyPlaceholder={emptyPlaceholder}
-          compactMode={compactMode}
-          autoFocus={autoFocus}
-        />
-      )}
-    </Adopt>
-  )
-
+const SearchBar = ({compactMode, autoFocus, onCancel}) => {
   return (
-    <div className={`vtex-top-menu__search-bar flex pa2-m flex-grow-1 justify-center ${mobileMode ? 'order-2' : 'order-1'}`}>
-      <div className="w-100 mw7">
-        {mobileMode ? !fixed && searchBar : searchBar}
+    <React.Fragment>
+      <div className="vtex-top-menu__search-bar flex pa2-m flex-grow-1 justify-center">
+        <div className="w-100 mw7">
+          <Adopt
+            mapper={{
+              placeholder: <FormattedMessage id='header.search-placeholder' />,
+              emptyPlaceholder: <FormattedMessage id='header.search-emptyPlaceholder' />,
+            }}
+          >
+            {({placeholder, emptyPlaceholder}) => (
+              <ExtensionPoint
+                id="search-bar"
+                placeholder={placeholder}
+                emptyPlaceholder={emptyPlaceholder}
+                compactMode={compactMode}
+                autoFocus={autoFocus}
+              />
+            )}
+          </Adopt>
+        </div>
       </div>
-    </div>
+      {compactMode && (
+        <Button size="small" variation="tertiary" onClick={onCancel}>
+          <FormattedMessage id="header.search-cancel" />
+        </Button>
+      )}
+    </React.Fragment>
   )
 }
 
 SearchBar.propTypes = {
-  mobileMode: PropTypes.bool,
-  fixed: PropTypes.bool,
+  onCancel: PropTypes.func,
   compactMode: PropTypes.bool,
   autoFocus: PropTypes.bool,
+}
+
+SearchBar.defaultProps = {
+  onCancel: () => {},
 }
 
 export default SearchBar
