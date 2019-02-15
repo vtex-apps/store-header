7c094b3a6a71e14e5eccf5ebd572bce4fafcdf9a
matheusps <matheus.ps@icloud.com>
2019-02-15 16:15:45 -0300

    Add new css handle for collapsible

--- react/components/Collapsible.js
+++ react/components/Collapsible.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import withScrollAnimation from './ScrollAnimation'
 import { Border } from './Helpers'
 import useDevice from '../hooks/useDevice'
+
+import header from '../store-header.css'
 
 const Collapsible = ({
   children,
@@ -14,9 +17,15 @@ const Collapsible = ({
   const { desktop } = useDevice()
   onAnimate(didAnimate)
 
+  const collapsibleClassnames = classNames(
+    animation,
+    header.topMenuCollapsible,
+    'relative bg-base'
+  )
+
   return desktop && !leanMode ? (
     <div
-      className={`${animation} relative bg-base`}
+      className={collapsibleClassnames}
       style={{
         zIndex: -1, // Animate under fixed content
         willChange: 'transform', // Better performance when animating
