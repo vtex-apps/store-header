fe6bc58e5ac67a90a1d64dd349c113524484d165
Lucas Bebber <lucasbbebber@gmail.com>
2019-06-04 11:23:57 -0300

    Add block class to ForceCenter

--- react/ForceCenter.js
+++ react/ForceCenter.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { generateBlockClass } from '@vtex/css-handles'
+import styles from './components/ForceCenter.css'
 
-const ForceCenter = ({ children }) => (
+const ForceCenter = ({ children, blockClass }) => (
   <div
-    className="absolute left-0 right-0 flex justify-center z-1"
+    className={generateBlockClass(
+      `${generateBlockClass(
+        styles.forceCenter,
+        blockClass
+      )} absolute left-0 right-0 flex justify-center z-1`
+    )}
     style={{ pointerEvents: 'none' }}
   >
     <div style={{ pointerEvents: 'all' }}>{children}</div>
