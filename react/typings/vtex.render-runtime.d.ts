3b07abdacf6c5cc05342b82e05a21b95634bbd1c
victorhmp <victor2142@gmail.com>
2019-08-24 17:04:34 -0300

    Add typings for VTEX apps

---
+++ react/typings/vtex.render-runtime.d.ts
@@ -0,0 +1,41 @@
+/* Typings for `render-runtime` */
+declare module 'vtex.render-runtime' {
+    import { Component, ReactElement } from 'react'
+  
+    export interface RenderContextProps {
+      runtime: {
+        account: string
+      }
+    }
+
+    export const Block: any
+    export const Link: any
+    export const NoSSR: any
+    export const RenderContextConsumer: ReactElement
+    export const canUseDOM: boolean
+    export const withRuntimeContext: any
+    export const Helmet: any
+    export const useRuntime: () => {
+      culture: {
+        locale: string
+      }
+      emitter: {
+        emit: (eventName: string, eventId: string) => void
+      }
+      query: {
+        app: string
+        filePath?: string
+      }
+      route: {
+        params: Record<string, string>
+        path: string
+      }
+      hints: {
+        desktop: boolean
+        mobile: boolean
+        phone: boolean
+        tablet: boolean
+      }
+    }
+  }
+  
---
+++ react/typings/vtex.sticky-layout.d.ts
@@ -0,0 +1,4 @@
+declare module 'vtex.sticky-layout' {
+  export const StickyRow
+  export const StickyRows
+}
