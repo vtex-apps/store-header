8b6427347b71ed1e5b74155914565f7772ded46a
Christian Kaisermann <christian@kaisermann.me>
2020-01-24 10:58:51 -0300

    Add useScrollOffset hook

---
+++ react/hooks/useScrollOffset.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import throttle from 'throttleit'
+
+const listeners = new Set<Function>()
+
+const scrollHandler = throttle(() => {
+  const scrollOffset = window.pageYOffset
+  listeners.forEach(set => set(scrollOffset))
+}, 100)
+
+function detach(fn: Function) {
+  listeners.delete(fn)
+  if (listeners.size === 0) {
+    window.removeEventListener('scroll', scrollHandler)
+  }
+}
+
+function attach(fn: Function) {
+  if (listeners.size === 0) {
+    window.addEventListener('scroll', scrollHandler)
+  }
+  listeners.add(fn)
+  return () => detach(fn)
+}
+
+/**
+ * Hook that handles the scroll position.
+ * It uses one single window scroll listener.
+ * @returns {Number} - scroll position value
+ */
+export const useScrollOffset = () => {
+  const [scroll, setScroll] = useState(0)
+  useEffect(() => attach(setScroll), [])
+  return { scroll }
+}
