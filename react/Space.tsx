a56790c77f33f0cace860e061e15e6cba396b47b
Christian Kaisermann <christian@kaisermann.me>
2020-12-10 11:35:17 -0300

    Use new css handle on Spacer

--- react/Space.tsx
+++ react/Space.tsx
@@ -1,10 +1,18 @@
+import type { PropsWithChildren } from 'react'
 import React from 'react'
+import type { CssHandlesTypes } from 'vtex.css-handles'
 import { useCssHandles } from 'vtex.css-handles'
 
 const CSS_HANDLES = ['headerSpacer'] as const
 
-function Spacer() {
-  const { handles } = useCssHandles(CSS_HANDLES)
+type Props = {
+  classes?: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
+}
+
+function Spacer({ classes }: PropsWithChildren<Props>) {
+  const { handles } = useCssHandles(CSS_HANDLES, {
+    classes,
+  })
 
   return <div className={`${handles.headerSpacer} flex flex-grow-1`} />
 }
